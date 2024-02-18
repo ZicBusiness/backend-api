ab61c5ddef53baa9872785a71c599e5104b565c7
ZicBusiness <zeroinfinitocriativo@gmail.com>
2024-02-18 15:51:01 -0300

    Primeiro

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs/promises');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const dataFilePath = './data.json';
+
+let formData = [];
+
+// Carregar dados do arquivo se existir
+try {
+  formData = require(dataFilePath);
+} catch (error) {
+  console.error('Error loading data file:', error);
+}
+
+app.post('/api/formData', async (req, res) => {
+  const newFormData = req.body;
+
+  // Adicionar novos dados à lista existente
+  formData.push(newFormData);
+
+  // Salvar os dados no arquivo
+  try {
+    await fs.writeFile(dataFilePath, JSON.stringify(formData, null, 2), 'utf-8');
+    console.log('Data saved to file successfully!');
+  } catch (error) {
+    console.error('Error saving data to file:', error);
+  }
+
+  res.json({ message: 'Dados salvos com sucesso!' });
+});
+
+app.get('/api/formData', (req, res) => {
+  res.json(formData);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
