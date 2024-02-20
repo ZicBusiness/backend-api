be77c3d4f7fa4112b87b737e2d88bb65915c3d02
ZicBusiness <zeroinfinitocriativo@gmail.com>
2024-02-20 16:20:49 -0300

    Cors

--- server.js
+++ server.js
@@ -3,8 +3,23 @@ const cors = require('cors');
 const fs = require('fs/promises');
 const app = express();
 
+// Configuração do CORS
+const corsOptions = {
+  origin: 'https://vercel-one-zicbusiness.vercel.app/', // Substitua pelo domínio do seu frontend
+  optionsSuccessStatus: 200, // Alguns navegadores podem exigir isso
+};
+
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`Received request: ${req.method} ${req.url}`);
+  next();
+});
+app.use((err, req, res, next) => {
+  console.error('Error caught globally:', err);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
 
 const dataFilePath = './data.json';
 
@@ -16,7 +31,7 @@ try {
 } catch (error) {
   console.error('Error loading data file:', error);
 }
-
+console.log('Before defining /api/formData route');
 app.post('/api/formData', async (req, res) => {
   console.log('Received POST request:', req.body);
   const newFormData = req.body;
@@ -34,6 +49,7 @@ app.post('/api/formData', async (req, res) => {
 
   res.json({ message: 'Dados salvos com sucesso!' });
 });
+console.log('After defining /api/formData route');
 
 app.get('/api/formData', (req, res) => {
   console.log('Endpoint accessed successfully.');
